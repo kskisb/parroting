c99e2b6608062f263b54e430c68e2ee7f1dda55e
kskisb <isbkosuke@gmail.com>
2025-02-01 04:15:11 +0900

    feat: google mapのapiを叩いて飲食店の情報をlineに送信する

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -6,16 +6,12 @@ class LineBotController < ApplicationController
       case event
       when Line::Bot::Event::Message
         case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: "text",
-            text: event.message["text"]
-          }
-          client.reply_message(event['replyToken'], message)
         when Line::Bot::Event::MessageType::Location
+          sendRestInfo(client, event)
+        else
           message = {
             type: "text",
-            text: "緯度: #{event.message['latitude']} 経度: #{event.message['longitude']}"
+            text: "左側の＋ボタンからレストランを検索したい場所の位置情報を送信してください。"
           }
           client.reply_message(event['replyToken'], message)
         end
@@ -30,5 +26,84 @@ class LineBotController < ApplicationController
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
+  end
+
+  def sendRestInfo(client, event)
+    lat = event.message['latitude']
+    lng = event.message['longitude']
+    reply_msg = getRestInfo(lat, lng)
+
+    puts "Reply message: #{reply_msg.inspect}"
+
+    message = {
+      type: "template",
+      altText: "レストラン一覧",
+      template: {
+        type: "carousel",
+        columns: reply_msg,
+        imageAspectRatio: "rectangle",
+        imageSize: "cover"
+      }
+    }
+
+    response = client.reply_message(event['replyToken'], message)
+    puts "LINE API Response: #{response.inspect}"
+  end
+
+  def getURL(place_id)
+    apikey = ENV["GOOGLE_MAPS_API_KEY"]
+    url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=#{place_id}&fields=url&key=#{apikey}"
+
+    # http GET リクエストを送信
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+
+    # JSONデコード
+    result = JSON.parse(response)
+
+    # URLを検出
+    return result["status"] == "OK" ? result["result"]["url"] : nil
+  end
+
+  def getRestInfo(lat, lng)
+    apikey = ENV["GOOGLE_MAPS_API_KEY"]
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{lng}&radius=1500&type=restaurant&key=#{apikey}&language=ja"
+
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    puts "Response: #{response}"
+
+    data = JSON.parse(response)
+
+    data["results"].each do |result|
+      # 店のurlを取得
+      result["url"] = getURL(result["place_id"])
+
+      # 評価順に並べ替えるための重み付け
+      result["rating"] = (result["rating"].to_f ** 2.5) * (result["user_ratings_total"].to_f ** 0.25)
+    end
+
+    # 評価順に並べ替え
+    sorted_results = data["results"].sort_by { |result| -result["rating"] }
+
+    columns = []
+    sorted_results.first(10).each do |shop|
+      addr = shop["vicinity"]
+      add = add[0, 40] if addr.length > 40
+
+      column = {
+        title: shop["name"],
+        text: addr,
+        actions: [
+          {
+            type: "uri",
+            label: "Google Mapで開く",
+            uri: shop["url"]
+          }
+        ]
+      }
+      columns << column
+    end
+    columns
   end
 end
